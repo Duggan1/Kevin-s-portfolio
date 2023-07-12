26002a8a97d03a28a81a5ce7e833c67c3c41f07a
Kevin Duggan <kevinduggan95@gmail.com>
2023-07-11 20:31:38 -0500

    end of night

--- src/About.js
+++ src/About.js
@@ -4,7 +4,7 @@ function About() {
     return (
         <>
       <div>
-        <h1>doing something </h1>
+      <h1 className='exsect3' style={{ fontWeight: 'bolder',fontSize:'90px',textAlign:'center',backgroundColor:'black',paddingTop:'10%',marginTop:'-7%',paddingBottom:'2%' }}>About Kevin</h1>
         <h2>ABOUT section</h2>
       </div>
       <div style={{height:'500px',marginLeft:"10px"}}className="ye"></div>
--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,55 @@
+import React, { useState } from 'react';
 
 function Home() {
-    return (
-        <>
-        <div style={{height:'200px',marginLeft:"35%"}}className="kevo"></div>
-      <div style={{textAlign:'center'}}>
-        <h1>Kevin Thomas Duggan </h1>
+  const [showVideo, setShowVideo] = useState(false);
+
+  const toggleVideoVisibility = () => {
+    setShowVideo(!showVideo);
+  };
+
+  return (
+    <>
+    <h1 className='exsect3' style={{ fontWeight: 'bolder',fontSize:'90px',textAlign:'center',backgroundColor:'black',paddingTop:'10%',marginTop:'-7%',paddingBottom:'2%' }}>Kevin Duggan</h1>
+    <h1 style={{textAlign:'center'}}>Kevin Thomas Duggan</h1>
+    <div style={{display: 'flex', justifyContent: 'center'}}>
+      <div style={{ height: '200px',display: 'flex', justifyContent: 'center'}}className="kevo"></div>
+      <div style={{ height: '200px',display: 'flex', justifyContent: 'center'}} className="kevio"></div></div>
+      <div style={{ textAlign: 'center' }}>
+        
         <h2>Computer Software Engineer</h2>
+        
+        <div style={{ display: 'inline-block'}}className="custom-checkbox">
+          <input
+            id="status"
+            type="checkbox"
+            name="status"
+            checked={showVideo}
+            onChange={toggleVideoVisibility}
+          />
+          <label htmlFor="status">
+            <div
+              className={`status-switch ${showVideo ? 'checked' : ''}`}
+              data-unchecked="Yes"
+              data-checked="No"
+            ></div>
+          </label>
+        </div><br></br>
+        {showVideo && (
+          <iframe
+            src="https://www.loom.com/embed/a5e33ab39fb040bea9ed716f4ac4fc9f?sid=c706cf97-5b76-43cc-a3f2-e264e8b4530f"
+            frameBorder="0"
+            allowFullScreen
+            title="kevin"
+            style={{ borderRadius: '25%', width: '710px', height: '400px', border: 'orange 5px solid', marginTop:'5%' }}
+          ></iframe>
+        )}
+
+        <h4 style={{fontWeight:'bolder'}}>Introduce Myself ?</h4>
       </div>
-      <div style={{height:'200px',marginLeft:"35%"}}className="kevio"></div>
-      </>
-    );
-  }
-  
-  export default Home;
+
+      
+    </>
+  );
+}
+
+export default Home;
